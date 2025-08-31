d81c2b5f20e0edb9624764d3a58a84d938e4597d
ivanpfaab <pfaabivan@gmail.com>
2025-08-31 15:34:11 -0300

    fix: cleaning log for test

--- client/common/client.go
+++ client/common/client.go
@@ -166,7 +166,6 @@ func (c *Client) queryWinners() error {
 		
 		// Successfully received winners
 		log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d", len(winners))
-		log.Infof("action: consulta_ganadores | result: success | ganadores: %v", winners)
 		gotWinners = true
 		return nil
 	}
